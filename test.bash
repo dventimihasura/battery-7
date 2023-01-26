a977abe406cd9717277cfe75f76cc8725785e553
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-01-26 14:52:24 -0800

    Automating the test scripts

--- test.bash
+++ test.bash
@@ -2,7 +2,17 @@
 
 set -x
 
-# docker-compose up -d
-# k6 run -u1 -d10s test.js --summary-export k6.log
-# docker-compose down
+docker-compose up -d
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8083/healthz)" != "200" ]]; do sleep 5; done
+pushd graphql-engine-1 && hasura deploy && popd
+pushd graphql-engine-2 && hasura deploy && popd
+pushd graphql-engine-3 && hasura deploy && popd
+pushd graphql-engine-4 && hasura deploy && popd
+pushd graphql-engine-5 && hasura deploy && popd
+k6 run -q -u1 -d10s test_1.js --summary-export test_1.json
+k6 run -q -u1 -d10s test_2.js --summary-export test_2.json
+k6 run -q -u1 -d10s test_3.js --summary-export test_3.json
+cat test_2.json | jq -r '.metrics.http_req_duration.avg'
+cat test_3.json | jq -r '.metrics.http_req_duration.avg'
+docker-compose down
 
---
+++ test_2.js
@@ -0,0 +1,7 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export default function () {
+    const headers = {'Content-Type': 'application/json'};
+    const query = `{"query":"query {product(id: ${Math.floor(Math.random()*10)+1}) {id name}}"}`
+    const res = http.post('http://127.0.0.1:8081/v1/graphql', query, {headers: headers});
+}
