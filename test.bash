32986da2c098b32a5f6615451cebff124f111174
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-01-26 15:18:30 -0800

    Finished the automation script

--- test.bash
+++ test.bash
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 
 set -x
+set -e
 
 docker-compose up -d
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8083/healthz)" != "200" ]]; do sleep 5; done
-pushd graphql-engine-1 && hasura deploy && popd
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8082/healthz)" != "200" ]]; do sleep 5; done
 pushd graphql-engine-2 && hasura deploy && popd
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8083/healthz)" != "200" ]]; do sleep 5; done
 pushd graphql-engine-3 && hasura deploy && popd
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8084/healthz)" != "200" ]]; do sleep 5; done
 pushd graphql-engine-4 && hasura deploy && popd
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8085/healthz)" != "200" ]]; do sleep 5; done
 pushd graphql-engine-5 && hasura deploy && popd
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8081/healthz)" != "200" ]]; do sleep 5; done
+pushd graphql-engine-1 && hasura deploy && popd
 k6 run -q -u1 -d10s test_1.js --summary-export test_1.json
 k6 run -q -u1 -d10s test_2.js --summary-export test_2.json
 k6 run -q -u1 -d10s test_3.js --summary-export test_3.json
